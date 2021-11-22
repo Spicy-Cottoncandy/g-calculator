d692ed74092cca3b8f0217574c9dc47ace92596f
Spicy-Cottoncandy <91308158+Spicy-Cottoncandy@users.noreply.github.com>
2021-11-23 03:09:27 +0900

    specialStatusの追加。

--- src/data/weapon/Claymore.js
+++ src/data/weapon/Claymore.js
@@ -14,11 +14,11 @@ export const Claymore = {
     refiningRank: []
   },
   2: {
-    name: "無工の剣2",
+    name: "松韻の響く頃",
     star: 5,
-    secondStatusType: "atkPercent",
-    baseATK: [46, 122, 153, 235, 266, 308, 340, 382, 414, 457, 488, 532, 563, 608],
-    secondStatus: [10.8, 19.1, 19.1, 27.8, 27.8, 32.2, 32.2, 36.5, 36.5, 40.9, 40.9, 45.3, 45.3, 49.6],
+    secondStatusType: "damageBonus.Physical",
+    baseATK: [49, 145, 176, 286, 317, 374, 406, 464, 495, 555, 586, 648, 679, 741],
+    secondStatus: [4.5, 8.0, 8.0, 11.6, 11.6, 13.4, 13.4, 15.2, 15.2, 17, 17, 18.9, 18.9, 20.7],
     refiningRank: []
   }
 };
--- src/data/weapon/Polearm.js
+++ src/data/weapon/Polearm.js
@@ -1,18 +1,18 @@
 export const Polearm = {
   1: {
-    name: "てすと",
+    name: "護摩の杖",
     star: 5,
-    secondStatusType: "atkPercent",
+    secondStatusType: "criticalDamage",
     baseATK: [46, 122, 153, 235, 266, 308, 340, 382, 414, 457, 488, 532, 563, 608],
-    secondStatus: [10.8, 19.1, 19.1, 27.8, 27.8, 32.2, 32.2, 36.5, 36.5, 40.9, 40.9, 45.3, 45.3, 49.6],
+    secondStatus: [14.4, 25.4, 25.4, 37.1, 37.1, 42.9, 42.9, 48.7, 48.7, 54.5, 54.5, 60.3, 60.3, 66.2],
     refiningRank: []
   },
   2: {
-    name: "てすと2",
+    name: "草薙の稲光",
     star: 5,
-    secondStatusType: "atkPercent",
+    secondStatusType: "energyRecharge",
     baseATK: [46, 122, 153, 235, 266, 308, 340, 382, 414, 457, 488, 532, 563, 608],
-    secondStatus: [10.8, 19.1, 19.1, 27.8, 27.8, 32.2, 32.2, 36.5, 36.5, 40.9, 40.9, 45.3, 45.3, 49.6],
+    secondStatus: [12, 21.2, 21.2, 30.9, 30.9, 35.7, 35.7, 40.6, 40.6, 45.4, 45.4, 50.3, 50.3, 55.1],
     refiningRank: []
   }
 };
